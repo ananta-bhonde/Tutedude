e02130ae9ea31e8f4a23c10e953ea35412b14324
ananta-bhonde <bhonde.ananta@gmail.com>
2026-02-23 21:18:47 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,6 +0,0 @@
-Number_1=int(input("Enter the first number: "))
-Number_2=int(input("Enter the second number: "))
-print(Number_1+Number_2)
-print(Number_1-Number_2)
-print(Number_1*Number_2)
-print(Number_1/Number_2)
